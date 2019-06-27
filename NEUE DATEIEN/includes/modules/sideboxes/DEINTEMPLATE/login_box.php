297c78884868aff57786dcc4e3c9d4d010870640
webchills <office@webchills.com>
2019-06-27 21:19:52 +0200

    unnötige Variable entfernt

--- NEUE DATEIEN/includes/modules/sideboxes/DEINTEMPLATE/login_box.php
+++ NEUE DATEIEN/includes/modules/sideboxes/DEINTEMPLATE/login_box.php
@@ -3,12 +3,10 @@
  * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
- * @version $Id: login_box.php 2019-06-27 07:54:04Z webchills $
+ * @version $Id: login_box.php 2019-06-27 21:19:04Z webchills $
  */
 
-if (($_GET['main_page'] != FILENAME_LOGIN && $_GET['main_page'] != FILENAME_CREATE_ACCOUNT && $_GET['main_page'] != FILENAME_PASSWORD_FORGOTTEN)) {
-
-      $login_box[] = TEXT_LOGIN_BOX;
+if (($_GET['main_page'] != FILENAME_LOGIN && $_GET['main_page'] != FILENAME_CREATE_ACCOUNT && $_GET['main_page'] != FILENAME_PASSWORD_FORGOTTEN)) {      
 
       if (!zen_is_logged_in()) {
             $title =  BOX_HEADING_LOGIN_BOX;
